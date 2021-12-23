a098c34e5bd4809a9547185298e886aeb28684dd
Lester <93441579+Lester-Leal@users.noreply.github.com>
2021-12-23 12:25:43 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// Configure your template paths
+//https://tailwindcss.com/docs/installation
+//https://tailwindcss.com/docs/installation/using-postcss
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
